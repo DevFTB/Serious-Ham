e3c0574865d1d8620a765c2f0b9f85ac98cc1d39
indegame <indegamedev@gmail.com>
2020-10-03 16:23:25 +1000

    BROKEN SHIT

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    private Vector3 Offset;
+    private Vector3 OriginalRotation;
+
+    public float Smoothing;
+
+    void Start()
+    {
+        Offset = transform.position - Target.position;
+        OriginalRotation = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Position = Vector3.Lerp(Target.position + Offset, transform.position, Smoothing * Time.deltaTime);
+
+        Vector3 lookPos = Target.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * Smoothing);
+
+        transform.position = Position;
+    }
+}
---
+++ Assets/MoveController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveController : MonoBehaviour
+{
+    public float ForwardAcceleration;
+    public float SidewaysTorque;
+
+    public float MaximumSpeed;
+    public float Stability;
+
+    private Rigidbody rb;
+    private bool OnGround;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (OnGround)
+        {
+            rb.AddRelativeTorque(Vector3.right * ForwardAcceleration * vertical, ForceMode.Acceleration);
+        }
+
+        Vector3 predictedUp = Quaternion.AngleAxis(
+           rb.angularVelocity.magnitude * Mathf.Rad2Deg * Stability,
+           rb.angularVelocity
+       ) * transform.up;
+
+        Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
+        // Uncomment the next line to stabilize on only 1 axis.
+        torqueVector = Vector3.Project(torqueVector, transform.forward);
+        rb.AddTorque(torqueVector);
+
+        //rb.AddRelativeTorque(Vector3.up * SidewaysTorque * horizontal, ForceMode.Acceleration);
+
+        //float PercentageFallenOver = transform.eulerAngles.z / 90;
+
+        //rb.AddTorque(Vector3.forward * -Mathf.Exp(PercentageFallenOver) * 5);
+        //Debug.Log(PercentageFallenOver);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            OnGround = true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            OnGround = false;
+        }
+    }
+}
