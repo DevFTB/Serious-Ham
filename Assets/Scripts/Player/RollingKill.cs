355655e8afadcdb9890d5c5e62777feafff29f09
Ryan <ryanwp526@gmail.com>
2020-10-04 18:03:50 +1100

    rolling killing done

---
+++ Assets/Scripts/Player/RollingKill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingKill : MonoBehaviour
+{
+    public int RollDamage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var OtherHealth = other.gameObject.GetComponent<Health>();
+        if(OtherHealth){
+            OtherHealth.TakeDamage(RollDamage);
+        }
+    }
+}
