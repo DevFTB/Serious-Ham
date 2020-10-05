eec7454975c681c600a5d0dcd8791e4d36ac89fa
Aditha Doratiyawa <indegamedev@gmail.com>
2020-10-06 00:39:47 +1100

    Main Menu Scene Updatad with Good GRPHIX

---
+++ Assets/CanvasGroupFader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class CanvasGroupFader : MonoBehaviour
+{
+    public float FadeDuration;
+
+    private CanvasGroup CG;
+    private Timer FadeTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CG = GetComponent<CanvasGroup>();
+        FadeTimer = new Timer(FadeDuration);
+
+        FadeTimer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeTimer.Update();
+
+        CG.alpha = FadeTimer.GetPercentageCompletion();
+    }
+}
