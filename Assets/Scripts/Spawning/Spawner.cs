814de5ab460e18e36dbad6c2d8f1831a0c114fb9
Ryan <ryanwp526@gmail.com>
2020-10-06 04:31:30 +1100

    spawning changes

--- Assets/Scripts/Spawning/Spawner.cs
+++ Assets/Scripts/Spawning/Spawner.cs
@@ -1,31 +1,45 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public bool ParentSpawnees;
 
     public GameObject Spawnee;
-    public int Quantity;
+    public int BaseQuantity;
     public int Separation;
+    public List<GameObject> Spawns;
 
-    public void Spawn()
+    private void Start()
     {
-        for (int i = 0; i < Quantity; i++)
+        Spawns = new List<GameObject>();
+    }
+
+    public void Spawn(float multiplier=1)
+    {
+        Spawns = Spawns.Where(spawn => spawn != null).ToList();
+        int length = Spawns.Count();
+        for (int i = 0; i < Mathf.Floor(multiplier * BaseQuantity) - length; i++)
         {
             float theta = (float)((2.0 * Mathf.PI) * Random.value);
 
             Vector3 separationVector = new Vector3(Mathf.Cos(theta), 0, Mathf.Sin(theta)) * Separation;
 
+            GameObject Spawn;
+
             if (ParentSpawnees)
             {
-                Instantiate(Spawnee, transform.position + separationVector, transform.rotation, gameObject.transform);
+                Spawn = Instantiate(Spawnee, transform.position + separationVector, transform.rotation, gameObject.transform);
 
             }
             else
             {
-                Instantiate(Spawnee, transform.position + separationVector, transform.rotation);
-
+                Spawn = Instantiate(Spawnee, transform.position + separationVector, transform.rotation);
             }
+
+            Spawns.Add(Spawn);
         }
 
     }
